ddc19231e5b35b08a1ed56152c897781aeefc905
carullahtursun <62027425+carullahtursun@users.noreply.github.com>
2022-10-10 15:30:28 +0300

    the end step

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ keys.addEventListener('click',function(e){
     if(!element.matches('button')) return ;
 
     if(element.classList.contains('operator')){
-        console.log('operator',element.value);
+        handleOperator(element.value);
+        updateDisplay();
         return;
     }
     if(element.classList.contains('decimal')){
@@ -43,20 +44,52 @@ keys.addEventListener('click',function(e){
 function handleOperator(nextOperator){
 
     const value =parseFloat(displayValue);
+
+    if(operator&&vFSV){
+        operator=nextOperator;
+        return;
+    }
     if(firstValue===null){
-       firstValue =nextOperator;  
+       firstValue =displayValue;  
+    }else if(operator){
+        const result =calculate(firstValue,value,operator);
+        displayValue=`${parseFloat(result.toFixed(7))}`;
+        firstValue=result;
     }
 
-    vFSV=true;
+    vFSV=true; 
     operator=nextOperator;
-    
-
 }
 
 
+function calculate(first,second,operator){
+
+    if(operator==='+'){
+        return first+second;
+    }else if(operator==='-'){
+        return first-second;
+    }
+    else if(operator==='*'){
+        return first*second;
+    }
+    else if(operator==='/'){
+        return first/second;
+    }
+
+    return second;
+
+}
+
 function inputNumber(number){
 
-    displayValue=displayValue==='0'? number:displayValue +number;
+    if(vFSV){
+        displayValue=number;
+        vFSV=false;
+    }else{
+        displayValue=displayValue==='0'? number:displayValue +number;
+    }
+
+    
 
 }
 
